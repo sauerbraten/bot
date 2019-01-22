b3f9911d0a09297a5d5d6cc2d4e9710659b2f23e
woozel <46923279+woozel@users.noreply.github.com>
2019-01-22 11:26:20 +0100

    Add files via upload

---
+++ my_bot.js
@@ -0,0 +1,55 @@
+const Discord = require('discord.js')
+const fetch = require("node-fetch");
+const HTMLParser = require('node-html-parser')
+
+const bot = new Discord.Client();
+
+bot.on('ready', () => {
+    console.log(`Logged in as ${bot.user.tag}!`)
+});
+
+bot.on('message', msg => {
+    if (msg.author.bot) {
+        // do not react to bot messages (including this bot's own messages)
+        return;
+    }
+
+    if (msg.content === '!ping') {
+        pong(msg)
+    } else if (msg.content.startsWith('!say ')) {
+        say(msg)
+    } else if (msg.content === '!fml') {
+        fml(msg)
+    } else if (msg.content.startsWith('!help')) {
+        msg.reply('Available commands: !help, !ping, !say, !fml\n\n!ping - makes the bot reply with a pong\n!say <any text> - makes the bot say the given text\n!fml - replies with a random post from fmylife.com')
+    }
+});
+
+bot.login('NTM3MDMzNDkxNTQwNjA2OTg3.DyfXqw.Ion1UImn28-JBEjGj5_CcWMthp4')
+
+// command implementations
+
+function pong(msg) {
+    msg.reply('Pong!')
+}
+
+function say(msg) {
+    msg.reply('I say: ' + msg.content.substr(5))
+}
+
+function fml(msg) {
+    const url = 'https://www.fmylife.com/random';
+    fetch(url)
+        .then(function (response) {
+            //success!
+            response.text().then(function(html) {
+                const root = HTMLParser.parse(html)
+                const firstPost = root.querySelector('.panel-content p a').text.trim()
+                msg.reply(firstPost)
+            })
+        })
+        .catch(function (err) {
+            console.log(err)
+            msg.reply('Could not fetch FML page!')
+        });
+}
